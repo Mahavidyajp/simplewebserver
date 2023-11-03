25a4af05e7228c28c63f6027ebc6c6a4da13dd86
Mahavidyajp <maviiispams17@gmail.com>
2023-11-04 00:06:41 +0530

    Success

--- snmp/web.py
+++ snmp/web.py
@@ -1,3 +1,5 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
 <html>
 	<title> Image Map </title>
 	<body>
@@ -36,5 +38,15 @@
 		</table>
 	</body>
 </html>
-
-				
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
